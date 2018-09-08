bb11b61134352bf8ef94702eb98d78c6a19d91d1
Kevin <kevin.velz@gmail.com>
2018-09-08 15:40:05 +0200

    Add Multilangual for German and English

--- plugin/src/main/java/me/velz/infinitypig/utils/FileManager.java
+++ plugin/src/main/java/me/velz/infinitypig/utils/FileManager.java
@@ -5,37 +5,48 @@ import lombok.Getter;
 import me.velz.infinitypig.InfinityPig;
 import me.velz.infinitypig.objects.InfinityPigEntity;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 
 public class FileManager {
-    
+
     private final InfinityPig plugin;
 
     public FileManager(InfinityPig plugin) {
         this.plugin = plugin;
     }
-    
+
+    @Getter
+    private String language;
+
     @Getter
     private ArrayList<String> names;
-    
+
     @Getter
     private final FileBuilder config = new FileBuilder("plugins/InfinityPig/", "config.yml");
-    
+
     public void loadConfig() {
         this.getConfig().load();
         this.names = new ArrayList<>();
+        this.language = this.getConfig().getString("language");
         this.getConfig().getStringList("infinitypig.names").forEach((name) -> {
             this.getNames().add(ChatColor.translateAlternateColorCodes('&', name));
         });
-        if(config.getConfiguration().contains("entities")) {
-            config.getConfiguration().getConfigurationSection("entities").getKeys(false).forEach((id) -> {
-                plugin.getEntities().put(id, new InfinityPigEntity(EntityType.valueOf(config.getString("entities." + id + ".type")), config.getLocation("entities." + id + ".location")));
-                plugin.getEntities().get(id).spawn();
-            });
-        }
+        Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+            @Override
+            public void run() {
+                if (config.getConfiguration().contains("entities")) {
+                    config.getConfiguration().getConfigurationSection("entities").getKeys(false).forEach((id) -> {
+                        plugin.getEntities().put(id, new InfinityPigEntity(EntityType.valueOf(config.getString("entities." + id + ".type")), config.getLocation("entities." + id + ".location")));
+                        plugin.getEntities().get(id).spawn();
+                    });
+                }
+            }
+        }, 200L);
     }
-    
+
     public void setDefaults() {
+        this.getConfig().addDefault("language", "en");
         this.getConfig().addDefault("infinitypig.names", new String[]{
             "&dHilfe, ich bin ein armes Schwein",
             "&dHalt! Bitte schlag mich nicht.",
@@ -45,6 +56,5 @@ public class FileManager {
         });
         this.getConfig().save();
     }
-    
-    
+
 }
--- plugin/src/main/java/me/velz/infinitypig/utils/MessageUtil.java
+++ plugin/src/main/java/me/velz/infinitypig/utils/MessageUtil.java
@@ -1,53 +1,57 @@
 package me.velz.infinitypig.utils;
 
 import lombok.Getter;
+import me.velz.infinitypig.InfinityPig;
 
 public enum MessageUtil {
-    
-    PREFIX("§f[§dInfinityPig§f] "),
+
+    PREFIX("", "§f[§dInfinityPig§f] ", "§f[§dInfinityPig§f] "),
     // Error Messages
-    ERROR_NOPERMISSIONS("§cDazu hast du keine Berechtigung."),
-    ERROR_PLAYERONLY("§cDieser Befehl kann nur von einem Spieler genutzt werden."),
-    ERROR_PLAYERNOTFOUND("§cDieser Spieler konnte nicht gefunden werden."),
-    ERROR_NONUMBER("§cDu musst eine gültige Zahl angeben."),
-    ERROR_NOITEMINHAND("§cDu hast kein Item in der Hand."),
-    ERROR_SYNTAX("§cSyntax Fehler, bitte nutze: %command"),
-    ERROR_ENTITYTYPENOTFOUND("§cDieser Entity Type konnte nicht gefunden werden."),
-    ERROR_NAMEISUSED("§cDiesen namen gibt es bereits."),
-    ERROR_NAMENOTFOUND("§cEs konnte kein Entity mit diesem namen gefunden werden."),
-    
-    COMMAND_HELP_HEAD("§6InfinityPig"),
-    COMMAND_HELP_RELOAD("§6/ipig reload §7Konfiguration neuladen"),
-    COMMAND_HELP_ADD("§6/ipig add [name] [type] §7Entity erstellen"),
-    COMMAND_HELP_REMOVE("§6/ipig remove [name] [type] §7Entity löschen"),
-    COMMAND_HELP_LIST("§6/ipig list §7Entities auflisten"),
-    COMMAND_ENTITYSPAWNED("§aEntity §e%name §awurde erstellt."),
-    COMMAND_ENTITYREMOVED("§aEntity §e%name §awurde entfernt."),
-    COMMAND_LISTENITIES("§aAlle Entities: §e%entities"),
-    COMMAND_NOENTITYFOUND("§cEs konnte kein Entity gefunden werden."),
-    COMMAND_RELOAD("§aKonfiguration wurde neugeladen.");
+    ERROR_NOPERMISSIONS("", "§cDazu hast du keine Berechtigung.", "§cYou dont have permissions."),
+    ERROR_PLAYERONLY("", "§cDieser Befehl kann nur von einem Spieler genutzt werden.", "§cThis command can only be used by a player."),
+    ERROR_PLAYERNOTFOUND("", "§cDieser Spieler konnte nicht gefunden werden.", "§cThis player could not be found."),
+    ERROR_SYNTAX("", "§cSyntax Fehler, bitte nutze: %command", "§cSyntax Error, please use: %command"),
+    ERROR_ENTITYTYPENOTFOUND("", "§cDieser Entity Type konnte nicht gefunden werden.", "§cThis entity type could not be found."),
+    ERROR_NAMEISUSED("", "§cDiesen namen gibt es bereits.", "§cThis name already exists."),
+    ERROR_NAMENOTFOUND("", "§cEs konnte kein Entity mit diesem namen gefunden werden.", "§cNo enity with this name could be found."),
+    COMMAND_HELP_HEAD("", "§6InfinityPig", "§6InfinityPig"),
+    COMMAND_HELP_RELOAD("", "§6/ipig reload §7Konfiguration neuladen", "§6/ipig reload §7Reload Configuration"),
+    COMMAND_HELP_ADD("", "§6/ipig add [name] [type] §7Entity erstellen", "§6/ipig add [name] [type] §7Create Entity"),
+    COMMAND_HELP_REMOVE("", "§6/ipig remove [name] [type] §7Entity löschen", "§6/ipig remove [name] [type] §7Delete Entity"),
+    COMMAND_HELP_LIST("", "§6/ipig list §7Entities auflisten", "§6/ipig list §7List all Entities"),
+    COMMAND_ENTITYSPAWNED("", "§aEntity §e%name §awurde erstellt.", "§aEntity §e%name §acreated."),
+    COMMAND_ENTITYREMOVED("", "§aEntity §e%name §awurde entfernt.", "§aEntity §e%name §aremoved."),
+    COMMAND_LISTENITIES("", "§aAlle Entities: §e%entities", "§aAll Entities: §e%entities"),
+    COMMAND_NOENTITYFOUND("", "§cEs konnte kein Entity gefunden werden.", "§cNo entity could be found."),
+    COMMAND_RELOAD("", "§aKonfiguration wurde neugeladen.", "§aConfiguration has been reloaded.");
 
     @Getter
-    private String local;
+    private String local, german, english;
 
-    private MessageUtil(String local) {
+    private MessageUtil(String local, String german, String english) {
         this.local = local;
+        this.german = german;
+        this.english = english;
     }
 
     public static void load() {
-        FileBuilder message = new FileBuilder("plugins/InfinityPig", "messages.yml");
+        FileBuilder message_en = new FileBuilder("plugins/InfinityPig", "messages_en.yml");
         for (MessageUtil m : MessageUtil.values()) {
-            message.addDefault("message." + m.toString().replaceAll("_", ".").toLowerCase(), m.local.replaceAll("§", "&"));
+            message_en.addDefault("message." + m.toString().replaceAll("_", ".").toLowerCase(), m.english.replaceAll("§", "&"));
         }
-        message.save();
+        message_en.save();
+        FileBuilder message_de = new FileBuilder("plugins/InfinityPig", "messages_de.yml");
+        for (MessageUtil m : MessageUtil.values()) {
+            message_de.addDefault("message." + m.toString().replaceAll("_", ".").toLowerCase(), m.german.replaceAll("§", "&"));
+        }
+        message_de.save();
+        FileBuilder message = new FileBuilder("plugins/InfinityPig", "messages_" + InfinityPig.getPlugin().getFileManager().getLanguage() + ".yml");
+        if(!message.getFile().exists()) {
+            message = new FileBuilder("plugins/InfinityPig", "messages_en.yml");
+        }
         for (MessageUtil m : MessageUtil.values()) {
             m.local = message.getConfiguration().getString("message." + m.toString().replaceAll("_", ".").toLowerCase()).replaceAll("&", "§");
         }
     }
 
-    public static void save(MessageUtil m) {
-        FileBuilder message = new FileBuilder("plugins/InfinityPig", "messages.yml");
-        message.save();
-    }
-    
 }
