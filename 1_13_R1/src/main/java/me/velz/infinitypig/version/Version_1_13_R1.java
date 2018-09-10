d50eff286babdcdf89c6c9dea6e9bef19b8f98d1
Kevin <kevin.velz@gmail.com>
2018-09-10 09:18:15 +0200

    Add support for Minecraft version 1_13_R1

---
+++ 1_13_R1/src/main/java/me/velz/infinitypig/version/Version_1_13_R1.java
@@ -0,0 +1,15 @@
+package me.velz.infinitypig.version;
+
+import me.velz.infinitypig.versions.Version;
+import org.bukkit.entity.Entity;
+
+public class Version_1_13_R1 implements Version {
+
+    @Override
+    public void disableAI(Entity entity) {
+        entity.getWorld().getLivingEntities().stream().filter((livingEntity) -> (livingEntity.getEntityId() == entity.getEntityId())).forEachOrdered((livingEntity) -> {
+            livingEntity.setAI(false);
+        });
+    }
+    
+}
